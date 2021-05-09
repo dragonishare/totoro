ba7c29a9aa863d6fe2b9771018417c30bb30a8ed
Dragon <532104+dragonishare@user.noreply.gitee.com>
2021-05-10 02:50:34 +0800

    目录结构调整

--- README.md
+++ README.md
@@ -27,3 +27,15 @@ yarn lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+### 目录结构
+
+* packages: 组件源码
+* website: 原src目录，改为组件文档官网，展示示例
+* website/docs: 组件文档和示例代码 markdown格式
+* src: 组件公用工具函数
+* src/theme: 组件样式文件夹（组件所有样式统一放这里）
+* src/index.ts: 组件统一导出入口文件（整体导出，全量包）
+
+
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,44 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
 module.exports = {
   devServer: {
     open: true,
+  },
+  pages: {
+    index: {
+      // page 的入口
+      entry: "website/main.ts",
+      // 模板来源
+      template: "public/index.html",
+      // 在 dist/index.html 的输出
+      filename: "index.html",
+      // 当使用 title 选项时
+    },
+  },
+
+  configureWebpack: {
+    resolve: {
+      // 设置别名
+      alias: {
+        "totoro-ui": path.join(__dirname, "./"),
+        packages: path.join(__dirname, "./packages"),
+        "@": path.join(__dirname, "./website"),
+        main: path.join(__dirname, "./src"),
+      },
+    },
+  },
+  // 扩展 webpack 配置，使 packages 加入编译
+  chainWebpack: (config) => {
+    config.module
+      .rule("js")
+      .include.add(path.resolve(__dirname, "packages"))
+      .end()
+      .use("babel")
+      .loader("babel-loader")
+      .tap((options) => {
+        // 修改它的选项...
+        return options;
+      });
   },
 };
